c7f664d31ff3a70f3d9def3b4ad57c726692fb35
Andrej730 <azhilenkov@gmail.com>
2025-04-15 23:48:28 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### BLauncher
+
+Simple script to start Blender Launcher with specific version of Blender.
+
+Example usage:
+```sh
+python blauncher.py -4.4 --background
+```
+
+Ideally it should set be set up as:
+```sh
+blauncher -4.4 --background
+```
+
+Ensure to set `BLENDER_LAUNCHER_REPO` enviroment variable to Blender Launcher repo:
+- https://github.com/Victor-IX/Blender-Launcher-V2
---
+++ blauncher.py
@@ -0,0 +1,36 @@
+import sys
+import subprocess
+import os
+from pathlib import Path
+
+
+def main():
+    # TODO: add default Blender version.
+    repo_location = os.environ.get("BLENDER_LAUNCHER_REPO")
+    if repo_location is None:
+        raise Exception("'BLENDER_LAUNCHER_REPO' environment variable is not set.")
+    repo_location = Path(repo_location)
+    main_script = repo_location / "source" / "main.py"
+    assert main_script.exists(), main_script
+
+    argv = sys.argv[1:]
+    assert len(argv) >= 1
+    assert argv[0].startswith("-")
+    version = argv[0].removeprefix("-")
+    blender_args = argv[1:]
+    args = [
+        "python",
+        str(main_script),
+        "launch",
+        "--version",
+        version,
+        "--cli",
+    ]
+    if blender_args:
+        args.extend(blender_args)
+    process = subprocess.run(args, cwd=repo_location)
+    exit(process.returncode)
+
+
+if __name__ == "__main__":
+    main()
