e3103ee0cb0ea7b90605985142f71b501e04b43f
Andrej730 <azhilenkov@gmail.com>
2025-04-16 10:22:05 +0500

    Add default (hardcoded) Blender version

--- README.md
+++ README.md
@@ -2,15 +2,28 @@
 
 Simple script to start Blender Launcher with specific version of Blender.
 
+blauncher acts exactly as if Blender was started directly,
+except it has additional optional first argument `version`
+that allows specifying Blender version to start
+(`py -x.x` inspired).
+
+`version` is a double digit version string provided in the format: `-x.y`.
+
 Example usage:
 ```sh
 python blauncher.py -4.4 --background
 ```
 
-Ideally it should set be set up as:
+Ideally it should be set up to be globally available as `blender`:
 ```sh
-blauncher -4.4 --background
+blender -4.4 --background
+blender --background
 ```
+
+### Setup
 
 Ensure to set `BLENDER_LAUNCHER_REPO` enviroment variable to Blender Launcher repo:
 - https://github.com/Andrej730/Blender-Launcher-V2/tree/launch-blender-args
+
+Currently default Blender version to start when version argument is not provided
+is hardcoded as `DEFAULT_BLENDER_VERSION`.
--- blauncher.py
+++ blauncher.py
@@ -1,11 +1,26 @@
 import sys
 import subprocess
 import os
+import re
 from pathlib import Path
+
+DEFAULT_BLENDER_VERSION = "4.4"
+
+
+def parse_version(argv: list[str]) -> tuple[str, list[str]]:
+    """
+    :return: Tuple of version and left out arguments.
+    """
+    # TODO: Ideally Blender Launcher should be able to work without version
+    # provided but currently it does not.
+    VERSION_PATTERN = r"-(\d.\d+)"
+    if not argv or not (re_match := re.match(VERSION_PATTERN, argv[0])):
+        return DEFAULT_BLENDER_VERSION, argv
+    version = re_match.group(1)
+    return version, argv[1:]
 
 
 def main():
-    # TODO: add default Blender version.
     repo_location = os.environ.get("BLENDER_LAUNCHER_REPO")
     if repo_location is None:
         raise Exception("'BLENDER_LAUNCHER_REPO' environment variable is not set.")
@@ -14,10 +29,8 @@ def main():
     assert main_script.exists(), main_script
 
     argv = sys.argv[1:]
-    assert len(argv) >= 1
-    assert argv[0].startswith("-")
-    version = argv[0].removeprefix("-")
-    blender_args = argv[1:]
+    version, blender_args = parse_version(argv)
+
     args = [
         "python",
         str(main_script),
