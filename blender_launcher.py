e0e245af572f8e829bc8a7cec0d7950edf2b8e2c
Andrej730 <azhilenkov@gmail.com>
2025-11-22 12:26:07 +0500

    blender_launcher.py - use `uv` for venv autodetection

--- blender_launcher.py
+++ blender_launcher.py
@@ -5,13 +5,18 @@ got to make it easier to launch Blender Launcher from terminal.
 """
 
 import os
+import shutil
 import subprocess
-import sys
 from pathlib import Path
+
+UV = shutil.which("uv")
+assert UV, "Could not find 'uv' executable in PATH."
 
 repo_location = os.environ.get("BLENDER_LAUNCHER_REPO")
 if repo_location is None:
     raise Exception("'BLENDER_LAUNCHER_REPO' environment variable is not set.")
 repo_location = Path(repo_location)
 main_script = repo_location / "source" / "main.py"
-subprocess.check_call([sys.executable, str(main_script)], cwd=repo_location)
+
+# Use UV to automatically pick up local venv.
+subprocess.check_call([UV, "run", str(main_script)], cwd=repo_location)
