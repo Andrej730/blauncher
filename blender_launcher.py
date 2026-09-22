387b57a6ad603eda99755386b6760bf7b60343cc
Andrej730 <azhilenkov@gmail.com>
2026-09-22 11:35:42 +0500

    Add pyproject.toml with scripts

--- blender_launcher.py
+++ blender_launcher.py
@@ -10,15 +10,23 @@ import shutil
 import subprocess
 from pathlib import Path
 
-UV = shutil.which("uv")
-assert UV, "Could not find 'uv' executable in PATH."
 
-repo_location = os.environ.get("BLENDER_LAUNCHER_REPO")
-if repo_location is None:
-    raise Exception("'BLENDER_LAUNCHER_REPO' environment variable is not set.")
-repo_location = Path(repo_location)
-main_script = repo_location / "source" / "main.py"
+def main():
+    UV = shutil.which("uv")
+    assert UV, "Could not find 'uv' executable in PATH."
 
-# Use UV to automatically pick up local venv.
-subprocess.check_call([UV, "run", str(repo_location / "build_style.py")], cwd=repo_location)
-subprocess.check_call([UV, "run", str(main_script)], cwd=repo_location)
+    repo_location = os.environ.get("BLENDER_LAUNCHER_REPO")
+    if repo_location is None:
+        raise Exception("'BLENDER_LAUNCHER_REPO' environment variable is not set.")
+    repo_location = Path(repo_location)
+    main_script = repo_location / "source" / "main.py"
+
+    # Use UV to automatically pick up local venv.
+    subprocess.check_call(
+        [UV, "run", str(repo_location / "build_style.py")], cwd=repo_location
+    )
+    subprocess.check_call([UV, "run", str(main_script)], cwd=repo_location)
+
+
+if __name__ == "__main__":
+    main()
